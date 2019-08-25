0b9cba2373f1715d610fa30e11e1b8361cd2bfe1
terryzvk <657926338@qq.com>
2019-08-25 14:53:00 +0800

    更换pg数据库

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_08_13_143616) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "microposts", force: :cascade do |t|
     t.string "title"
     t.text "content"
